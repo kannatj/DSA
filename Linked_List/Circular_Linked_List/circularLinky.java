252f3f1e9f1f25bc62010282faf129ecb0d571aa
TJ kanna <35217198+kannatj@users.noreply.github.com>
2020-01-13 08:24:07 +0530

    Updated Circluar LL

--- Linked_List/Circular_Linked_List/circularLinky.java
+++ Linked_List/Circular_Linked_List/circularLinky.java
@@ -1,108 +1,131 @@
-package circularLinkedList;
+    package linkedList;
 
-public class CircularLinky {
-	
+public class circularLinky {
+
 	Node head;
-	class Node {
-		int data;
+	public class Node{
 		Node next;
-		
-		Node(int d){
-			data = d;
-		}
+		int data;
 	}
 	
 	public void insert(int data) {
-		Node new_node = new Node(data);
-		Node tmp = head;
-		
+		Node itr = head;
+		Node new_node = new Node();
 		new_node.data = data;
-		//new_node.next = head;
 		
-		if(head != null) {
-			while(tmp.next != head) {
-				tmp = tmp.next;
-			}
-			
-			tmp.next = new_node;
-			new_node.next = head;
+		
+		if(head == null) {
+			head = new_node;
+			new_node.next = new_node;
 		}
 		else {
-			new_node.next = new_node;
-            head = new_node;		
+			
+		   while(itr.next != head){
+				itr = itr.next;
+		   }
+			
+			itr.next = new_node;
+			new_node.next = head;
 		}
 		
-		//head = new_node;
-		return;
-	}
-	
-	public void print() {
-		Node itr = head;
-
-		
-		if(head!=null)
-		do {
-		  System.out.println(itr.data);
-		  itr = itr.next;
-		}while(itr!= head);
-		
-		return;
 	}
 	
 	public void delete(int data) {
-		Node itr = head;
-		Node prev;
+		Node itr = head,prev = null;
 		
-		if(head != null) {
-			if(itr.data == data) {
-				head = head.next;
-			}
+		
+		//Iterating and identifying the previous and next position of the data
+		while(itr.data != data) {
+			if(itr.next == head) {
+				System.out.println("No elements found");
+			    break;
+		    }
+			prev = itr;                // Previous node
+			itr = itr.next;            // Next node
 		}
+		
+		
+		//If data is the only node
+		if(itr.next == head && prev == null) {
+			head = null;
+			return;
+		}
+		
+		//If data is in head and have some nodes in addition
+		if(itr == head) {
+			prev = head;
 			
+			while(prev.next != head) {
+				prev = prev.next;
+			}
+			
+			head = head.next;
+			prev.next = head;
+		}
+		
+		
+		//If data is in the last node 
+		else if (itr.next == head && prev != null) {
+		    prev.next = head;	
+		}
+		
+		
+		//Elsewhere in the middle
+		else {
+			prev.next = itr.next;
+		}
+		
+		
 	}
+	
+	
+	public void print() {
+		Node itr = head;
+		if(head != null) {
+			System.out.println("Printing list: \n");
+			do {
+				System.out.print(itr.data+" ");
+				itr = itr.next;
+			}while(itr != head);
+	 }
+	}
+	
 	
 	public void count() {
 		Node itr = head;
-		int count = 0;
-		if(head!= null) {
-			do{
+		int count = 0 ;
+		
+		if(head == null)
+			System.out.println("Count "+ count);
+		else {
+			 do{
 				count++;
 				itr = itr.next;
-			}while(itr != head);
-			System.out.println("No of nodes in the circular linked list is "+count);
-		}
-		else {
-			System.out.println("Empty Linkedlist");
+			}while(itr!= head); 
+			System.out.println("Count "+ count);
 		}
 	}
 	
-	public void linked_to_circular() {
+	void SlToCl() {
 		Node itr = head;
-		while(itr.next!=null) {
+		
+		while(itr.next != null) {
 			itr = itr.next;
 		}
-		
 		itr.next = head;
 	}
-	public static void main(String as[]) {
-		CircularLinky cl = new CircularLinky(); 
-		cl.count();
-		for(int i =1;i<=5;i++)
-			cl.insert(i);
-		
-//		cl.insert(10);
-//		cl.insert(20);
-//		cl.insert(30);
-//		cl.insert(40);
-//		cl.insert(50);
-//		cl.insert(60);
-		cl.print();
-//cl.insert(70);
-		cl.delete(1);
-		cl.count();
-		cl.print();
-		System.out.println("Head :"+cl.head.data);
-	}
 	
-
+	public static void main(String as[]) {
+		Circular cl = new Circular();
+		for(int i =1;i<6;i++) {
+		   cl.insert(i);
+		}
+		
+		cl.print();
+		cl.count();
+		cl.delete(5);
+		cl.print();
+		cl.delete(1);
+		cl.print();
+	}
 }
