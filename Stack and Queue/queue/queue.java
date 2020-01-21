e7a1550cbacd933f1f031ae51403dc2884cd4398
TJ kanna <35217198+kannatj@users.noreply.github.com>
2020-01-21 07:43:56 +0530

    queue implementation using linked list

--- Stack and Queue/queue/queue.java
+++ Stack and Queue/queue/queue.java
@@ -1,1 +1,54 @@
+package queue;
+//queue implementation using linked list
+public class queue{
+int queue[];
+node front, rear;
 
+queue(){
+	front = rear = null;
+}
+
+class node{
+	int data;
+	node next;
+}
+
+public void enqueue(int data) {
+	node newNode = new node();
+	newNode.data = data;
+	
+	System.out.println(data+" entered the queue");
+	
+	if(rear ==  null) {
+		front = rear = newNode;
+		return;
+	}
+	
+	rear.next = newNode;
+	rear = newNode;
+}
+
+public void dequeue() {
+	if(rear == null) {
+		System.out.println("queue is empty");
+	}
+	else {
+		System.out.println("Dequed element is"+front.data);
+		front = front.next;
+	}
+	
+	if(front == null)
+		rear = null;
+}
+
+
+public static void main(String as[]) {
+	queue q = new queue();
+	q.enqueue(10);
+	q.enqueue(20);
+	q.enqueue(30);
+	q.dequeue();
+	q.dequeue();
+	
+}
+}
